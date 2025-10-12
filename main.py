31ddd39ce9719cc5f397a42d234a5e73525c3da9
oheyek <oheyek@proton.me>
2025-10-12 11:54:37 +0200

    add URL cleaning method and update video retrieval logic for improved YouTube link handling

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main() -> None:
     Main function of the program.
     """
     video_info = VideoInfo()
-    print(video_info.get_video_info("https://www.youtube.com/watch?v=dQw4w9WgXcQ"))
+    print(video_info.get_video_info("https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1"))
 
 if __name__ == "__main__":
     main()
