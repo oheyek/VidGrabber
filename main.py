9a26d089d2ef175e204ec9213134a43ffaf153c2
oheyek <oheyek@proton.me>
2025-10-13 12:45:02 +0200

    Reformat code for readability and type annotations

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ def main() -> None:
     Main function of the program.
     """
     video_info = VideoInfo()
-    for info in video_info.get_video_info("https://youtu.be/dQw4w9WgXcQ?si=SPIt6zX1gx9NouFZ"):
+    for info in video_info.get_video_info(
+        "https://youtu.be/dQw4w9WgXcQ?si=SPIt6zX1gx9NouFZ"
+    ):
         print(info)
 
 
