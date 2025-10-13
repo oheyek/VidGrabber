c95c7eda2049d8cf30a6e91d91d06edf410f1e13
oheyek <oheyek@proton.me>
2025-10-13 13:23:38 +0200

    refactor main function and test cases for improved readability and consistency, add a function for listing available video qualities

--- main.py
+++ main.py
@@ -6,10 +6,8 @@ def main() -> None:
     Main function of the program.
     """
     video_info = VideoInfo()
-    for info in video_info.get_video_info(
-        "https://youtu.be/dQw4w9WgXcQ?si=SPIt6zX1gx9NouFZ"
-    ):
-        print(info)
+    for information in video_info.get_video_info("https://youtu.be/dQw4w9WgXcQ?si=52ngrNGc_WNyEkUb"):
+        print(information)
 
 
 if __name__ == "__main__":
