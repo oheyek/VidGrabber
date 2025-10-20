05a5f37c33e9c69aa3c68bd59dec1a5afe6c2028
oheyek <oheyek@proton.me>
2025-10-20 12:35:02 +0200

    add tag extractor class with functionality to extract and save YouTube video tags

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from downloader import Downloader
 from thumbnail_downloader import ThumbnailDownloader
 from video_info import VideoInfo
+from tag_extractor import TagExtractor
 
 LINK = "https://youtu.be/dQw4w9WgXcQ?si=52ngrNGc_WNyEkUb"
 
@@ -12,6 +13,7 @@ def main() -> None:
     video_info: VideoInfo = VideoInfo()
     downloader: Downloader = Downloader(video_info)
     thumbnail_downloader: ThumbnailDownloader = ThumbnailDownloader(video_info)
+    tag_extractor: TagExtractor = TagExtractor(video_info)
 
     for information in video_info.get_video_info(LINK):
         print(information)
@@ -20,6 +22,7 @@ def main() -> None:
 
     print(downloader.download_audio(LINK, "mp3"))
     print(thumbnail_downloader.download_thumbnail(LINK))
+    print(tag_extractor.extract_tags(LINK))
 
 
 if __name__ == "__main__":
