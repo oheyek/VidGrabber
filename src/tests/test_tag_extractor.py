6c431deeddfd6b9843739e364c14f1bf2e21a858
oheyek <oheyek@proton.me>
2025-11-16 11:35:57 +0100

    feat: add fixture for tag_extractor tests

--- src/tests/test_tag_extractor.py
+++ src/tests/test_tag_extractor.py
@@ -3,8 +3,13 @@ import pytest
 from src.tag_extractor import TagExtractor
 from src.video_info import VideoInfo
 
-video_info = VideoInfo()
-tag_extractor = TagExtractor(video_info)
+@pytest.fixture
+def tag_extractor() -> TagExtractor:
+    """
+    Fixture that provides TagExtractor instance for tests.
+    :return: TagExtractor instance for tests.
+    """
+    return TagExtractor(VideoInfo())
 
 VALID_YOUTUBE_URLS = [
     "https://youtu.be/dQw4w9WgXcQ?si=52ngrNGc_WNyEkUb",
@@ -24,27 +29,27 @@ INVALID_YOUTUBE_URLS = [
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize("url", VALID_YOUTUBE_URLS)
-async def test_extract_tags_valid_links(url: str) -> None:
+async def test_extract_tags_valid_links(url: str, tag_extractor) -> None:
     """Test for extracting tags with valid links"""
     result = await tag_extractor.extract_tags(url)
     assert result == "Tags saved to file and copied to clipboard!"
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize("url", INVALID_YOUTUBE_URLS)
-async def test_extract_tags_invalid_links(url: str) -> None:
+async def test_extract_tags_invalid_links(url: str, tag_extractor) -> None:
     """Test for extracting tags with invalid links"""
     result = await tag_extractor.extract_tags(url)
     assert result == "Invalid link provided."
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize("invalid_input", [None, 123, [], {}, 12.34])
-async def test_extract_tags_invalid_input_types(invalid_input) -> None:
+@pytest.mark.parametrize("invalid_input", [None, 123, 12.34])
+async def test_extract_tags_invalid_input_types(invalid_input, tag_extractor) -> None:
     """Test for extracting tag with invalid YouTube URLs."""
     result = await tag_extractor.extract_tags(invalid_input)
     assert result == "Invalid link provided."
 
 @pytest.mark.asyncio
-async def test_extract_tags_empty_url() -> None:
+async def test_extract_tags_empty_url(tag_extractor) -> None:
     """Test that empty string is rejected while downloading."""
     result = await tag_extractor.extract_tags("")
     assert  result == "Invalid link provided."
