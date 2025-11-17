a667d33479da8c33cba86e205d289cee1a573d2e
oheyek <oheyek@proton.me>
2025-11-17 22:12:18 +0100

    chore: clean up main module and improve CI setup for cross-platform binary handling

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 import asyncio
+import sys
 
 from src.ui.interface import AppUI
 from src.updater import initialize_binaries
-
-LINK = "https://youtu.be/dQw4w9WgXcQ?si=52ngrNGc_WNyEkUb"
-LINK2 = "https://youtu.be/njX2bu-_Vw4?si=VgF6vfdYZFh9dhPe"
-LINK3 = "https://youtu.be/xOMMV_qXcQ8?si=q5pqhGOqFbcr4acG"
-
-import sys
 
 if sys.platform == 'win32':
     import os
