d0ebc913f249c97d738f1b02bf8c7ab899b72b75
oheyek <oheyek@proton.me>
2025-11-17 22:01:46 +0100

    refactor: remove unused imports and commented code from main module

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import asyncio
 
-from src.queue.download_queue import DownloadQueue
 from src.ui.interface import AppUI
 from src.updater import initialize_binaries
-from src.video_info import VideoInfo
 
 LINK = "https://youtu.be/dQw4w9WgXcQ?si=52ngrNGc_WNyEkUb"
 LINK2 = "https://youtu.be/njX2bu-_Vw4?si=VgF6vfdYZFh9dhPe"
@@ -13,9 +11,11 @@ import sys
 
 if sys.platform == 'win32':
     import os
+
     os.environ['PYTHONIOENCODING'] = 'utf-8'
     sys.stdout.reconfigure(encoding='utf-8', errors='replace')
     sys.stderr.reconfigure(encoding='utf-8', errors='replace')
+
 
 async def main() -> None:
     """
@@ -27,44 +27,6 @@ async def main() -> None:
     print()
     interface: AppUI = AppUI()
     interface.mainloop()
-
-    # video_info: VideoInfo = VideoInfo()
-    # queue: DownloadQueue = DownloadQueue()
-
-    # for information in await video_info.get_video_info(LINK):
-    #     print(information)
-
-    # print(queue.add_video(LINK, 240))
-    # print(queue.add_video(LINK, 360))
-    # print(queue.add_video(LINK2, 240))
-    # print(queue.add_video(LINK3, 240))
-
-    # print(queue.add_mp3_audio(LINK))
-    # print(queue.add_mp3_audio(LINK2))
-    # print(queue.add_mp3_audio(LINK3))
-
-    # print(queue.add_wav_audio(LINK))
-    # print(queue.add_wav_audio(LINK2))
-    # print(queue.add_wav_audio(LINK3))
-
-    # print(queue.add_thumbnail(LINK))
-    # print(queue.add_thumbnail(LINK2))
-    # print(queue.add_thumbnail(LINK3))
-
-    # print(queue.add_tags(LINK))
-    # print(queue.add_tags(LINK2))
-    # print(queue.add_tags(LINK3))
-
-    # results = await asyncio.gather(
-    #     queue.start_queue("mp4"),
-    #     queue.start_queue("mp3"),
-    #     queue.start_queue("wav"),
-    #     queue.start_queue("jpg"),
-    #     queue.start_queue("csv"),
-    # )
-
-    # for result in results:
-    #     print(result)
 
 
 if __name__ == "__main__":
